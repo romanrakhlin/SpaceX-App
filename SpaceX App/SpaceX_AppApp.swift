c0a8b6ebeee08bc90176b211455074fad18ab84c
Roman Rakhlin <rrakhlin@gmail.com>
2022-03-29 18:49:14 +0300

    Initial Commit

---
+++ SpaceX App/SpaceX_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceX_AppApp.swift
+//  SpaceX App
+//
+//  Created by Roman Rakhlin on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SpaceX_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
